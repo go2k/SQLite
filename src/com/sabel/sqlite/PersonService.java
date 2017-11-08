bc0dff4d93e4b3c234e958721dc3316608d41a29
Michael König <m.koenig@go2k.de>
2017-11-08 11:11:23 +0100

    ..

---
+++ src/com/sabel/sqlite/PersonService.java
@@ -0,0 +1,35 @@
+package com.sabel.sqlite;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class PersonService {
+
+    private static final String url="jdbc:sqlite:d:\\Test.sqlite";
+    private Connection connection;
+    private Statement statement;
+
+    public PersonService() throws SQLException{
+        this.connection = DriverManager.getConnection(url);
+        this.statement = connection.createStatement();
+    }
+
+    public void save(Person person) throws SQLException {
+        String sql = "insert into person VALUES (" + person.getId() + ",'" + person.getNachname() + "','" + person.getJahrgang() + "')");
+        this.statement.executeQuery(sql);
+    }
+
+    public void close() throws SQLException {
+        if (statement != null) {
+            statement.close();
+            statement = null;
+        }
+        if (connection != null) {
+            connection.close();
+            connection = null;
+        }
+    }
+
+}
